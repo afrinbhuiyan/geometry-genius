14cc352fdfb67f79d0e23e150feb6c8e972532fc
afrinbhuiyan <mstafrinbhuiyan@gmail.com>
2024-11-02 12:12:40 +0600

    added a htmlCss

--- rhombus-area.js
+++ rhombus-area.js
@@ -1,6 +1,28 @@
+// function calculateRhombusArea(){
+//     const baseRadius = getInputValueById('rhombus-base');
+//     const heightRadius = getInputValueById('rhombus-height');
+//     const area = 0.5 * baseRadius * heightRadius;
+//     setInnerTextById('rhombus-area', area)
+// }
+
+
 function calculateRhombusArea(){
-    const baseRadius = getInputValueById('rhombus-base');
-    const heightRadius = getInputValueById('rhombus-height');
-    const area = 0.5 * baseRadius * heightRadius;
-    setInnerTextById('ellipse-area', area);
+    const base = getInputValueById('rhombus-base');
+    const height = getInputValueById('rhombus-height');
+    // console.log(perimeter, apothem);
+    const area = 0.5 * base * height;
+    setInnerTextById('rhombus-area', area);
+}
+
+function getInputValueById(inputFieldId){
+    const inputField = document.getElementById(inputFieldId);
+    const inputValueText = inputField.value;
+    const value = parseFloat(inputValueText);
+    return value;
+
+}
+
+function setInnerTextById (elementId, area){
+    const element = document.getElementById(elementId);
+    element.innerText = area;
 }
