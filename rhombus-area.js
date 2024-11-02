89d56bfb3c78bcbcff4c6bab8ffe4e99d98580a1
afrinbhuiyan <mstafrinbhuiyan@gmail.com>
2024-11-02 11:41:48 +0600

    added a commit

--- rhombus-area.js
+++ rhombus-area.js
@@ -2,5 +2,5 @@ function calculateRhombusArea(){
     const baseRadius = getInputValueById('rhombus-base');
     const heightRadius = getInputValueById('rhombus-height');
     const area = 0.5 * baseRadius * heightRadius;
-    setInnerTextById('rhombus-area', area);
+    setInnerTextById('ellipse-area', area);
 }
